59a879f7207abbe316c672f0d9c9c1527d9ac538
Megha Priyadarshini <meghapriyadarshini68@gmail.com>
2019-01-08 15:35:34 +0530

    tweak google strategy settings

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,6 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: 'mongodb://localhost/emailyprod',
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
+    googleRedirectURI: 'https://secret-journey-85240.herokuapp.com'
 }
