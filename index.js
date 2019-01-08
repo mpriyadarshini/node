b4666eb1ffb5346582b851f86ffded6743b5f71f
Megha Priyadarshini <meghapriyadarshini68@gmail.com>
2019-01-08 14:52:21 +0530

    finished auth flow

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 
-app.get('/',(req,res) =>{
-    res.send({
-        hi:'there'
-    });
+app.use(express.static('public'));
 
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, (err) => {
+    if(err) {
+        console.error(err);
+    } else {
+        console.log('Server started on port '+PORT);
+    }
+});
